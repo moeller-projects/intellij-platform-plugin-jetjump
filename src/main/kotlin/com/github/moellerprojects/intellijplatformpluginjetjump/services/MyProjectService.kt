df2ca51c9f6eeadd687c5395a8c18c29300d6fdc
GitHub Action <action@github.com>
2025-03-07 12:52:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/moellerprojects/intellijplatformpluginjetjump/services/MyProjectService.kt
+++ src/main/kotlin/com/github/moellerprojects/intellijplatformpluginjetjump/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.moellerprojects.intellijplatformpluginjetjump.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.moellerprojects.intellijplatformpluginjetjump.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
